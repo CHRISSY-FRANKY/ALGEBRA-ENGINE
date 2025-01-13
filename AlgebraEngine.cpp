2d34ea4d23371c6712f47cedf39cb8ce44062563
Christopher Francisco <cjfg00gl3@gmail.com>
2025-01-12 19:22:38 -0600

    CREATE AlgebraEngine.cpp

---
+++ AlgebraEngine.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class AlgebraEngine
+{
+public:
+    AlgebraEngine()
+    {
+        std::cout << "Hello, World!" << std::endl;
+    }
+private:
+};
+
+int main(void)
+{
+    AlgebraEngine ae;
+    return 0;
+}
